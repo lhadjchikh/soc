702ec81a18f89c8f6e9c2c7fe38df0e4446f17b9
Leila Hadj-Chikh <lhadjchikh@users.noreply.github.com>
2025-07-08 10:23:53 -0400

    feat: add API endpoint stubs

--- app.py
+++ app.py
@@ -3,6 +3,13 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/soc-stock")
+async def get_soc_stock():
+    """Get SOC stock value at given coordinates."""
+    pass
+
+
+@app.get("/stats")
+async def get_stats():
+    """Get SOC summary statistics for the entire SOC Stock dataset."""
+    pass
